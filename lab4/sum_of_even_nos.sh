93cf1e56b7d87d6b019ed17f468c96590c5ce431
Shikha N <56836929+shiks25@users.noreply.github.com>
2020-10-15 15:05:05 +0530

    prog2

---
+++ lab4/sum_of_even_nos.sh
@@ -0,0 +1,18 @@
+#Shell script to  find the SUM OF EVEN NUMBERS UPTO N using Looping construct.
+
+#!/bin/sh
+
+echo "To find the sum of n even numbers"
+echo "Enter the value of n: \c"
+read num
+
+sum=0
+x=0
+while [ $x -le $num ]  
+do
+	sum=`expr $sum + $x`
+	x=`expr $x + 2`
+done
+
+echo $sum
+
